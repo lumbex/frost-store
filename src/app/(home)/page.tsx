31d7605dc716b51fa513c940808d18b38f38df30
lumbex <olumbex@gmail.com>
2025-05-27 14:59:21 +0100

    home carousel

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,6 @@
+import { HomeCarousel } from '@/components/shared/home/home-carousel'
+import data from '@/lib/data'
+
 export default async function Page() {
-    return (
-        <div>
-            <h1 className='h1-bold text-center p-10 text-red'>Home Page Content</h1>
-            <div className="border border-border p-4">
-                Hello border!
-            </div>
-        </div>
-    )
+    return <HomeCarousel items={data.carousels} />
 }
